d1605163d3c7fa5dbd7312d63bc8e3ec2a3c9e03
Loïc Frering <loic.frering@gmail.com>
2013-08-07 18:39:34 +0200

    Add grunt-contrib-jshint to hint src and test files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,12 +3,16 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      src: ['Gruntfile.js', 'src/**/*.js', '!src/libs/**', 'tests/**/*.js']
+    },
     qunit: {
       all: ['tests/**/*.html']
     }
   });
 
   // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-qunit');
 
   // Default task(s).
